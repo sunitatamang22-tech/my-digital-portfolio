215fada364f3401bcb944cd5b3d73c4de8663bff
sunitatamang22-tech <sunitapakhrin8494@gmail.com>
2025-07-02 18:29:39 +0930

    chnaged

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -36,7 +36,7 @@ export default function AboutPage() {
               <div className="space-y-4">
                 <p className="text-muted-foreground">
                   hello this is changed 
-                  hello this is test
+                  <br>hello this is test </br>
                 I’m an aspiring Cybersecurity Analyst with a deep curiosity for how things work—and how to protect them when they break. 
                 Originally from overseas and now based in Adelaide, I’m currently pursuing my Master’s in Cybersecurity at the University of the Sunshine Coast (UniSC).
                 </p>
